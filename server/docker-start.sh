5a7bde0896d44ceb96b182a76f2a2101a3fcc107
Najib Fauzan <najib.fauzan@gyssteel.com>
2026-04-09 04:22:57 +0000

    [ci skip]

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -2,7 +2,7 @@
 
 echo "🧹 Clearing OneDrive cache..."
 
-node server/scripts/clear-onedrive-cache.js || echo "⚠️ Clear cache skipped"
+node scripts/clear-onedrive-cache.js || echo "⚠️ Clear cache skipped"
 
 echo "🚀 Starting server..."
 exec node server.js
